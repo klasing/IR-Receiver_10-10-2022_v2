3c7617dba087f3fc5927fee59cf19dc993c41350
klasing1159 <klasing1159@gmail.com>
2023-10-31 00:12:39 +0100

    bla

--- Stm32_Peripheral_w_TabControl_25-10-2023_v1/Stm32_Peripheral_w_TabControl_25-10-2023_v1.cpp
+++ Stm32_Peripheral_w_TabControl_25-10-2023_v1/Stm32_Peripheral_w_TabControl_25-10-2023_v1.cpp
@@ -1,22 +1,39 @@
 // Stm32_Peripheral_w_TabControl_25-10-2023_v1.cpp : Defines the entry point for the application.
 //
 
+//****************************************************************************
+//*                     include
+//****************************************************************************
 #include "framework.h"
 #include "Stm32_Peripheral_w_TabControl_25-10-2023_v1.h"
 
+//****************************************************************************
+//*                     define
+//****************************************************************************
 #define MAX_LOADSTRING 100
 
+//****************************************************************************
+//*                     global
+//****************************************************************************
 // Global Variables:
-HINSTANCE hInst;                                // current instance
+HINSTANCE g_hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
+HWND g_hDlg = { 0 };
+
+//****************************************************************************
+//*                     prototype
+//****************************************************************************
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+//****************************************************************************
+//*                     wWinMain
+//****************************************************************************
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -26,6 +43,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // TODO: Place code here.
+// load common controls: tab, listview, header, toolbar, statusbar,
+    // and tooltip
+    INITCOMMONCONTROLSEX ic = { 0 };
+    ic.dwSize = sizeof(INITCOMMONCONTROLSEX);
+    ic.dwICC = ICC_BAR_CLASSES | ICC_LISTVIEW_CLASSES;
+    BOOL bSuccessInit = InitCommonControlsEx(&ic);
 
     // Initialize global strings
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -45,6 +68,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // Main message loop:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
+        if (IsDialogMessage(g_hDlg, &msg))
+        {
+            continue;
+        }
         if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
         {
             TranslateMessage(&msg);
@@ -57,11 +84,9 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 
 
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
+//****************************************************************************
+//*                     MyRegisterClass
+//****************************************************************************
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
     WNDCLASSEXW wcex;
@@ -83,22 +108,22 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     return RegisterClassExW(&wcex);
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
+//****************************************************************************
+//*                     InitInstance
+//****************************************************************************
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+   g_hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   HWND hWnd = CreateWindowW(szWindowClass
+       , szTitle
+       , WS_OVERLAPPEDWINDOW
+       , 10//CW_USEDEFAULT
+       , 10//0
+       , 800//CW_USEDEFAULT
+       , 600//0
+       , nullptr, nullptr, hInstance, nullptr
+   );
 
    if (!hWnd)
    {
@@ -111,16 +136,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE: Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
+//****************************************************************************
+//*                     WndProc
+//****************************************************************************
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -132,7 +150,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             switch (wmId)
             {
             case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                DialogBox(g_hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
                 break;
             case IDM_EXIT:
                 DestroyWindow(hWnd);
@@ -160,6 +178,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 }
 
 // Message handler for about box.
+//****************************************************************************
+//*                     About
+//****************************************************************************
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
     UNREFERENCED_PARAMETER(lParam);
--- Stm32_Peripheral_w_TabControl_25-10-2023_v1/framework.h
+++ Stm32_Peripheral_w_TabControl_25-10-2023_v1/framework.h
@@ -8,6 +8,14 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
 #include <windows.h>
+// an easy way to add the common control library
+#pragma comment(lib, "comctl32.lib")
+//////////////////////////////////////////////////////////////////////////////
+//#pragma comment(linker, "\"/manifestdependency:type='win32' \
+//name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+//processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+//////////////////////////////////////////////////////////////////////////////
+#include <CommCtrl.h>
 // C RunTime Header Files
 #include <stdlib.h>
 #include <malloc.h>
