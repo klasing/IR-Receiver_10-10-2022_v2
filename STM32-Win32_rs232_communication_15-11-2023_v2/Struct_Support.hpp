b6c239f92e4a004dc78d03c44151fd9cb6c9138f
klasing1159 <klasing1159@gmail.com>
2023-11-21 23:01:35 +0100

    bla

---
+++ STM32-Win32_rs232_communication_15-11-2023_v2/Struct_Support.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+//****************************************************************************
+//*                     include
+//****************************************************************************
+#include "framework.h"
+#include "resource.h"
+
+//****************************************************************************
+//*                     FRAME
+//****************************************************************************
+typedef struct tagFRAME
+{
+	const CHAR soh;					// 1
+	UINT16 cmd;						// 2
+	const CHAR stx;					// 1
+	CHAR payload[LEN_MAX_ENTRY];	// 32
+	const CHAR etx;					// 1
+	const CHAR etb;					// 1
+	const CHAR eot;					// 1
+} FRAME, *PFRAME;					// 39 byte total
